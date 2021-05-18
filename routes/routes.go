604f5e985ccd8d117a6f67c239434624f1ab553b
DevelopNaoki <develop.hamamura@gmai.com>
2021-05-18 15:50:14 +0900

    edit template file and routes

--- routes/routes.go
+++ routes/routes.go
@@ -9,9 +9,13 @@ func Router(engine *gin.Engine) {
 	engine.Static("/css", "src/css")
 	engine.Static("/js", "src/js")
 
+	engine.GET("/overview", func(c *gin.Context) {
+                c.HTML(http.StatusOK, "dashboad.tmpl", gin.H {
+                        "title": "Overview",
+                })
+        })
 	engine.GET("/dashboad", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "dashboad.tmpl", gin.H {
-			"sidebar": "dashboad",
 			"title": "Dashboad",
 		})
 	})
